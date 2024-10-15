1453fb67dd24154d72ff72e48451849185d2c643
Justin Herndon <jherndon111@gmail.com>
2024-10-14 21:22:24 -0500

    creating function stubs for implementation

--- download.c
+++ download.c
@@ -1,1 +1,15 @@
 #include "download.h"
+
+struct xml_data download_and_parse(url){
+
+	return ;
+}
+
+struct show_information (xml_input,file_name){
+	return ;
+}
+
+void create_torrent_file(show_information){
+
+}
+
